8f376f679a5b75467bdace7c531684232acb95a2
Imprimus <70212746+imprimus-dev@users.noreply.github.com>
2020-09-02 14:10:50 +0530

    Project Rubicon - initial commit

---
+++ rubicon/src/main/java/com/rubicon/RubiconApplication.java
@@ -0,0 +1,13 @@
+package com.rubicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubiconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubiconApplication.class, args);
+	}
+
+}
